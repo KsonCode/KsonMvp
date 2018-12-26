f1417e94e4eac4e9a4158f27038181b9e329b1e3
KsonCode <19655910@qq.com>
2018-12-26 17:02:12 +0800

    Update MainActivity.java

--- app/src/main/java/com/kson/ksonmvp/MainActivity.java
+++ app/src/main/java/com/kson/ksonmvp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //我是新增的功能
+        //woshi xiaowang
     }
 }
